084e3dd0bac3c3f5486db53e9bbeb8e416ba8c58
FadeIn64 <danik6052@gmail.com>
2024-02-03 17:49:05 +0400

    Correct call usages

--- src/main/java/su/fedin/nn/Tracer.java
+++ src/main/java/su/fedin/nn/Tracer.java
@@ -1,17 +1,31 @@
 package su.fedin.nn;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Tracer {
     private static final String HEADER = "EPOCH\tRIGHTS\tERROR\n";
     private static final String LINE_FORMAT= "%d\t%d\t%f\n";
+
+    private static Map<String, Tracer> pool = new HashMap<>();
+
+    static Tracer getTracer(NeuralNetwork nn){
+        Tracer tracer = pool.get(nn.name);
+        if(tracer == null){
+            tracer = new Tracer(nn.name);
+            pool.put(nn.name, tracer);
+        }
+        return tracer;
+    }
+
     private String path;
     private String name;
     private File traceFile;
+    private FileOutputStream out;
 
     Tracer(String path, String name) {
         this.path = path;
@@ -32,10 +46,17 @@ public class Tracer {
         }
 
         try {
-            FileOutputStream out = new FileOutputStream(traceFile, false);
+            out = new FileOutputStream(traceFile, false);
             out.write(HEADER.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -46,10 +67,17 @@ public class Tracer {
     void trace(int epoch, int right, double error){
         String line = String.format(LINE_FORMAT, epoch, right, error);
         try {
-            FileOutputStream out = new FileOutputStream(traceFile, true);
+            out = new FileOutputStream(traceFile, true);
             out.write(line.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
